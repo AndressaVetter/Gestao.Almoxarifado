202ce3ef29e10ee6517aab16b8e8932f076eb24c
aluno <aluno@lab01-07>
2026-05-11 09:19:49 -0300

    11/05/26

---
+++ Almoxarifado/src/Categoria.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Categoria {
+    
+    private String nomeCategoria;
+    private String descricaoCategoria;
+    
+    public String nomeCategoria() {
+        return nomeCategoria;
+    }
+    public void nomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+    public String descricaoCategoria (){
+        return descricaoCategoria;
+    }
+    public void descricaoCategoria (String descricaoCategoria){
+        this.descricaoCategoria = descricaoCategoria;
+    }
+}
