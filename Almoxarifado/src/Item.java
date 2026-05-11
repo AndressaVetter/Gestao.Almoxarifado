52bb78d644d7518113036fd8d120152088f6f439
aluno <aluno@lab01-07>
2026-05-11 09:08:39 -0300

    11/05/26

---
+++ Almoxarifado/src/Item.java
@@ -0,0 +1,24 @@
+
+import java.util.Set;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Item {
+   private String nomeItem;
+   private String descricacaoItem;
+   private String catgoriaItem;
+   private int unidadeMedidaItem;
+   private int quantidadeAtualItem;
+   private int quantidadeMinimaItem;
+   private String localizaçãoItem;
+   private String statusItem;
+        
+     
+}
