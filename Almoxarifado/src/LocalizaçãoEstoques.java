c0303d8e6caeb8ede2c4fb6632de2efb60815893
aluno <aluno@lab01-07>
2026-05-11 09:32:25 -0300

    11/05/26

---
+++ Almoxarifado/src/LocalizaçãoEstoques.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class LocalizaçãoEstoques {
+    
+    private String descricaoFornecedor;
+    private String setor;
+    
+    public String getdescricaoFornecedor(){
+        return descricaoFornecedor;
+    }
+    public void setdescricaoFornecedor(String descricaoFornecedor){
+        this.descricaoFornecedor = descricaoFornecedor;
+    }
+            public String getSetor(){
+                return setor;
+            }
+            public void setSetor (String setor){
+                this.setor = setor;
+            }
+}
