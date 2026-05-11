f442a214bf0b5d60ea95610601e9963c8085fd41
aluno <aluno@lab01-07>
2026-05-11 09:14:30 -0300

    11/05/26

--- Almoxarifado/src/Item.java
+++ Almoxarifado/src/Item.java
@@ -19,6 +19,70 @@ public class Item {
    private int quantidadeMinimaItem;
    private String localizaçãoItem;
    private String statusItem;
+
+    public String getNomeItem() {
+        return nomeItem;
+    }
+
+    public void setNomeItem(String nomeItem) {
+        this.nomeItem = nomeItem;
+    }
+
+    public String getDescricacaoItem() {
+        return descricacaoItem;
+    }
+
+    public void setDescricacaoItem(String descricacaoItem) {
+        this.descricacaoItem = descricacaoItem;
+    }
+
+    public String getCatgoriaItem() {
+        return catgoriaItem;
+    }
+
+    public void setCatgoriaItem(String catgoriaItem) {
+        this.catgoriaItem = catgoriaItem;
+    }
+
+    public int getUnidadeMedidaItem() {
+        return unidadeMedidaItem;
+    }
+
+    public void setUnidadeMedidaItem(int unidadeMedidaItem) {
+        this.unidadeMedidaItem = unidadeMedidaItem;
+    }
+
+    public int getQuantidadeAtualItem() {
+        return quantidadeAtualItem;
+    }
+
+    public void setQuantidadeAtualItem(int quantidadeAtualItem) {
+        this.quantidadeAtualItem = quantidadeAtualItem;
+    }
+
+    public int getQuantidadeMinimaItem() {
+        return quantidadeMinimaItem;
+    }
+
+    public void setQuantidadeMinimaItem(int quantidadeMinimaItem) {
+        this.quantidadeMinimaItem = quantidadeMinimaItem;
+    }
+
+    public String getLocalizaçãoItem() {
+        return localizaçãoItem;
+    }
+
+    public void setLocalizaçãoItem(String localizaçãoItem) {
+        this.localizaçãoItem = localizaçãoItem;
+    }
+
+    public String getStatusItem() {
+        return statusItem;
+    }
+
+    public void setStatusItem(String statusItem) {
+        this.statusItem = statusItem;
+    }
         
-     
+
 }
