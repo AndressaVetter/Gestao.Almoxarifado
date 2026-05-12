0b03f3d656b32d6b03fa4e1fee7b7743dc160376
aluno <aluno@lab01-07>
2026-05-12 13:13:37 -0300

    12/05/26

---
+++ GestaoAmorarifado/src/main/java/ifc/gestaoamorarifado/Fornecedor.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Fornecedor {
+    
+    private String nome;
+    private int cnpj;
+    private int numeroContato;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public int getNumeroContato() {
+        return numeroContato;
+    }
+
+    public void setNumeroContato(int numeroContato) {
+        this.numeroContato = numeroContato;
+    }
+    
+    
+}
