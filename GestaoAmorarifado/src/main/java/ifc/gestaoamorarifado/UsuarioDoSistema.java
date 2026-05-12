b1b4c17407af486f466b668ec6cfa1e58346d08c
aluno <aluno@lab01-07>
2026-05-12 13:14:53 -0300

    12/05/26

---
+++ GestaoAmorarifado/src/main/java/ifc/gestaoamorarifado/UsuarioDoSistema.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifc.gestaoamorarifado;
+
+/**
+ *
+ * @author aluno
+ */
+public class UsuarioDoSistema {
+    
+    private String login;
+    private int senha;
+    private String perfil;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+}
