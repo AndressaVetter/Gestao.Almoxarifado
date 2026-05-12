c97eb01a95387c5f66168a0083fd161b7aa14a19
aluno <aluno@lab01-07>
2026-05-12 13:13:54 -0300

    12/05/26

---
+++ GestaoAmorarifado/src/main/java/ifc/gestaoamorarifado/Item.java
@@ -0,0 +1,88 @@
+
+import java.util.Set;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Item {
+   private String nomeItem;
+   private String descricacaoItem;
+   private String catgoriaItem;
+   private int unidadeMedidaItem;
+   private int quantidadeAtualItem;
+   private int quantidadeMinimaItem;
+   private String localizaçãoItem;
+   private String statusItem;
+
+    public String getNomeItem() {
+        return nomeItem;
+    }
+
+    public void setNomeItem(String nomeItem) {
+        this.nomeItem = nomeItem;
+    }
+
+    public String getDescricacaoItem() {
+        return descricacaoItem;
+    }
+
+    public void setDescricacaoItem(String descricacaoItem) {
+        this.descricacaoItem = descricacaoItem;
+    }
+
+    public String getCatgoriaItem() {
+        return catgoriaItem;
+    }
+
+    public void setCatgoriaItem(String catgoriaItem) {
+        this.catgoriaItem = catgoriaItem;
+    }
+
+    public int getUnidadeMedidaItem() {
+        return unidadeMedidaItem;
+    }
+
+    public void setUnidadeMedidaItem(int unidadeMedidaItem) {
+        this.unidadeMedidaItem = unidadeMedidaItem;
+    }
+
+    public int getQuantidadeAtualItem() {
+        return quantidadeAtualItem;
+    }
+
+    public void setQuantidadeAtualItem(int quantidadeAtualItem) {
+        this.quantidadeAtualItem = quantidadeAtualItem;
+    }
+
+    public int getQuantidadeMinimaItem() {
+        return quantidadeMinimaItem;
+    }
+
+    public void setQuantidadeMinimaItem(int quantidadeMinimaItem) {
+        this.quantidadeMinimaItem = quantidadeMinimaItem;
+    }
+
+    public String getLocalizaçãoItem() {
+        return localizaçãoItem;
+    }
+
+    public void setLocalizaçãoItem(String localizaçãoItem) {
+        this.localizaçãoItem = localizaçãoItem;
+    }
+
+    public String getStatusItem() {
+        return statusItem;
+    }
+
+    public void setStatusItem(String statusItem) {
+        this.statusItem = statusItem;
+    }
+        
+
+}
