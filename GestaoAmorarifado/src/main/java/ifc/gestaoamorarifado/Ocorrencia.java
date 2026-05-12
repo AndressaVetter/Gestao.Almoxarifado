7ede0937ea9fe7528bfb8c7b5458862dbe5c3a67
aluno <aluno@lab01-07>
2026-05-12 13:14:40 -0300

    12/05/26

---
+++ GestaoAmorarifado/src/main/java/ifc/gestaoamorarifado/Ocorrencia.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifc.gestaoamorarifado;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Ocorrencia {
+     
+    private String tipo;
+    private Date data;
+    private String hora;
+    private String local;
+    private  String descricao;
+    private String status;
+    private String prioridade;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+    
+}
